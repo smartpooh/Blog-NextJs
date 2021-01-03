67d81a19c118aaadb792609b5b125c3a980de2c4
smartpooh <smartperson0318@gmail.com>
2021-01-03 03:06:43 -0500

    global css

--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -1,3 +1,5 @@
-export default function App({Component, pagesProps}) {
+import styles from '../styles/global.css'
+
+export default function App({Component, pageProps}) {
     return <Component {...pageProps} />
 }
